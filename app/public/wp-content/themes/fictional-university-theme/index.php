8bca36784cd016fa65c8d77936ebbae945a020b4
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-02 14:31:21 -0300

    Added title and content of posts dynamically in index.php

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,9 @@
-This is our amazing custom theme.
+<?php
+    while(have_posts()){
+        the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        <hr>
+    <?php
+    }
+?>
