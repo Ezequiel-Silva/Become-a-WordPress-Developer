a0ee30d9c6f4fd9cd479ec62b29bcb50608880ff
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-04 14:21:35 -0300

    Message in the subtitle to remind you to change it later

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -7,7 +7,7 @@
             <div class="page-banner__content container container--narrow">
                 <h1 class="page-banner__title"><?php the_title(); ?></h1>
                 <div class="page-banner__intro">
-                    <p>Learn how the school of your dreams got started.</p>
+                    <p>DONT FORGET TO REPLACE ME LATER</p>
                 </div>
             </div>
         </div>
