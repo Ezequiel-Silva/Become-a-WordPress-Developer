dc7716b3d7e2e94b61aa9a9c5218f9ecf80e2cf8
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-04 17:09:51 -0300

    Function that returns an echo with double 4 in index.php

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,5 +1,12 @@
     <?php get_header(); ?>
 
+    <?php 
+        function doubleMeEcho($x){
+            echo $x * 2,'<br>';
+        } 
+        doubleMeEcho(4);
+    ?>
+
     <div class="page-banner">
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>)"></div>
         <div class="page-banner__content container t-center c-white">
