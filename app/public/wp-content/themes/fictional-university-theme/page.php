ca5b536284e511f0415d47b2085bd64b809f5fb5
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-04 14:12:27 -0300

    Adding dynamic content in generic-content class of áge.php

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -14,22 +14,21 @@
 
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+                <p>
+                    <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+                </p>
+            </div>
 
-        <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="#">About Us</a></h2>
+                <ul class="min-list">
+                    <li class="current_page_item"><a href="#">Our History</a></li>
+                    <li><a href="#">Our Goals</a></li>
+                </ul>
+            </div>
 
-        <div class="generic-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+            <div class="generic-content">
+                <?php the_content(); ?>
             </div>
         </div>
     <?php }
