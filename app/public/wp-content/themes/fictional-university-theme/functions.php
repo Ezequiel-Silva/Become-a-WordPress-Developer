3e76bf1089a915daa450bff30b13131e728c0d08
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-04 14:39:18 -0300

    Adding the title in the browser tab by the functions.php file

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -7,4 +7,8 @@
         wp_enqueue_style('university_extra_styles',get_theme_file_uri('/build/index.css'));
     }
     add_action('wp_enqueue_scripts','university_files');
+    function university_features(){
+        add_theme_support('title-tag');
+    }
+    add_action('after_setup_theme','university_features');
 ?>
