59c80b816e867ff38c16a19aacde2a7ea33a62a2
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-04 16:42:14 -0300

    Creating variable $theParent to get the dynamic id of the post's parent in page.php

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -13,10 +13,12 @@
         </div>
 
         <div class="container container--narrow page-section">
-            <?php if(wp_get_post_parent_id(get_the_ID())){ ?>
+            <?php
+            $theParent = wp_get_post_parent_id(get_the_ID());
+            if($theParent){ ?>
                 <div class="metabox metabox--position-up metabox--with-home-link">
                     <p>
-                        <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(x); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+                        <a class="metabox__blog-home-link" href="<?php get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
                     </p>
                 </div>
             <?php } ?>
