22d8d0270c017596f4b4b44430246fd5df66fab5
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-05 14:37:31 -0300

    Character set and mobile viewport in header.php

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -1,10 +1,8 @@
 <html>
     <head>
         <meta charset="
-        <?php bloginfo('charset');?>"/>
-        <meta name="viewport" 
-        content="width=device-width, 
-        initial-scale=1">
+        <?php bloginfo( 'charset' ); ?>" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1" />
         <?php wp_head();?>
     </head>
     <body>
