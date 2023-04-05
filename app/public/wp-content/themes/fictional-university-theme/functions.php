696ba6745f4bdad32022281454644709ab188531
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-05 16:41:58 -0300

    Register navigation menu in functions.php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -8,6 +8,7 @@
     }
     add_action('wp_enqueue_scripts','university_files');
     function university_features(){
+        register_nav_menu('headerMenuLocation','Header Menu Location');
         add_theme_support('title-tag');
     }
     add_action('after_setup_theme','university_features');
