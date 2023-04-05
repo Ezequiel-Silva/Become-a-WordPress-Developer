6ae005b3e505bbd0908f27e428131cc31720443b
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-05 09:20:20 -0300

    Function to generate all website pages in page.php

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -26,8 +26,7 @@
             <div class="page-links">
                 <h2 class="page-links__title"><a href="#">About Us</a></h2>
                 <ul class="min-list">
-                    <li class="current_page_item"><a href="#">Our History</a></li>
-                    <li><a href="#">Our Goals</a></li>
+                    <?php wp_list_pages(); ?>
                 </ul>
             </div>
 
