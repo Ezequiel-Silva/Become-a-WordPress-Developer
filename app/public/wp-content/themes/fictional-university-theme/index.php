b32db059ec8456164fc583c4dcb892a76011552e
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-06 11:25:06 -0300

    Adding page-banner structure in index.php

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,11 @@
-This is the generic blog listing screen template.
+<?php get_header(); ?>
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">Welcome to our blog!</h1>
+        <div class="page-banner__intro">
+            <p>Keep up with our latest news.</p>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
