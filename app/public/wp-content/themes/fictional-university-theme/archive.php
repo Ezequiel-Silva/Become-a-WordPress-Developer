bd41dc20ca3fef2353e29f34d5a1075569b857b1
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-10 14:19:43 -0300

    Dynamic title and description in archive.php

--- app/public/wp-content/themes/fictional-university-theme/archive.php
+++ app/public/wp-content/themes/fictional-university-theme/archive.php
@@ -3,14 +3,10 @@
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">
-            <?php if(is_category()){
-                single_cat_title();
-            } if(is_author()) {
-                echo "Posts by "; the_author();
-            } ?>
+            <?php the_archive_title(); ?>
         </h1>
         <div class="page-banner__intro">
-            <p>Keep up with our latest news.</p>
+            <p><?php the_archive_description(); ?></p>
         </div>
     </div>
 </div>
