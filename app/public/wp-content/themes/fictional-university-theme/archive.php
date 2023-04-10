9ac87fbfdb2c2b5f92ae1cbfd11b09f990505748
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-10 13:24:00 -0300

    If it is a category it displays a message, if it is an author it displays another message in the archive.php

--- app/public/wp-content/themes/fictional-university-theme/archive.php
+++ app/public/wp-content/themes/fictional-university-theme/archive.php
@@ -2,7 +2,13 @@
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to our blog!</h1>
+        <h1 class="page-banner__title">
+            <?php if(is_category()){
+                echo "category name will go here";
+            } if(is_author()) {
+                echo "author name will go here";
+            } ?>
+        </h1>
         <div class="page-banner__intro">
             <p>Keep up with our latest news.</p>
         </div>
