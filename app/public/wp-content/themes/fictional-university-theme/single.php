99ed2af38041af1b2cffaf3540be3c5073d3a644
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-10 10:57:46 -0300

    Creating initial metabox structure

--- app/public/wp-content/themes/fictional-university-theme/single.php
+++ app/public/wp-content/themes/fictional-university-theme/single.php
@@ -12,7 +12,17 @@
             </div>
         </div>
         <div class="container container--narrow page-section">
-            metabox will go here
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php get_permalink($theParent) ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i> 
+                        Back to <?php echo get_the_title($theParent); ?>
+                    </a> 
+                    <span class="metabox__main">
+                        <?php the_title(); ?>
+                    </span>
+                </p>
+            </div>
             <div class="generic-content">
                 <?php the_content(); ?>
             </div>
