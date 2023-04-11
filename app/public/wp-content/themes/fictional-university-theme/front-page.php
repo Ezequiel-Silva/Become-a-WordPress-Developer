5c8a36cc01264a42a24813685266ff2fbd151702
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-11 10:03:32 -0300

    Clear custom query after executed in front-page.php

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -58,7 +58,7 @@
                         <p><?php echo wp_trim_words(get_the_content(),18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                     </div>
                 </div>
-            <?php } ?>
+            <?php } wp_reset_postdata(); ?>
 
             <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
