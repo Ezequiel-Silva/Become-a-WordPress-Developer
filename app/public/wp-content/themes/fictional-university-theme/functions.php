c03863848ce1f5b391fece464bc1a9272d6be4eb
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-11 14:23:00 -0300

    Adding custom post for event in functions.php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -14,4 +14,10 @@
         add_theme_support('title-tag');
     }
     add_action('after_setup_theme','university_features');
+    function university_post_types(){
+        register_post_type('event', array(
+            'public' => true
+        ));
+    }
+    add_action('init','university_post_types');
 ?>
