33b66c093936ca9603e98a0a7aaa148f09f15afb
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-11 16:27:06 -0300

    Modern screen of block editor for event post type in university-post-types.php

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,7 @@
     function university_post_types(){
         register_post_type('event', array(
             'public' => true,
+            'show_in_rest' => true,
             'labels' => array(
                 'name' => 'Events',
                 'add_new_item' => 'Adicionar novo evento',
