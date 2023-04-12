d0dd0130406f0f8241a2d44521a0a6c120793eba
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-12 13:16:16 -0300

    Modying subtitle in archive-event.php

--- app/public/wp-content/themes/fictional-university-theme/archive-event.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-event.php
@@ -4,7 +4,7 @@
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">All Events</h1>
         <div class="page-banner__intro">
-            <p><?php the_archive_description(); ?></p>
+            <p>See what is going on in our world.</p>
         </div>
     </div>
 </div>
