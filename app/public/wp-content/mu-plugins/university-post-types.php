2827ee79bfcd765856d64989d9e2ea5f2d84f14d
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-12 12:06:17 -0300

    Parameter to change slug in university-post-types.php, and creation of archive-event.php file

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
     function university_post_types(){
         register_post_type('event', array(
+            'rewrite' => array('slug' => 'events'),
             'has_archive' => true,
             'public' => true,
             'show_in_rest' => true,
