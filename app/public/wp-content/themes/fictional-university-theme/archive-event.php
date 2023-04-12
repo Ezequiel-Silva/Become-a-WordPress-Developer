27caecfd0b4af08ec42f47159f0f05d8306f8b73
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-12 13:22:20 -0300

    Putting event-summary in the post loop in archive-event.php

--- app/public/wp-content/themes/fictional-university-theme/archive-event.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-event.php
@@ -10,16 +10,14 @@
 </div>
 <div class="container container–narrow page-section">
     <?php while(have_posts()){ the_post(); ?>
-        <div class="post-item">
-            <h2 class="headline headline--medium headline--post-title">
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </h2>
-            <div class="metabox">
-                <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
-            </div>
-            <div class="generic-content">
-                <?php the_excerpt(); ?>
-                <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+        <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+                <span class="event-summary__month">Mar</span>
+                <span class="event-summary__day">25</span>
+            </a>
+            <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php echo wp_trim_words(get_the_content(),18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
             </div>
         </div>
     <?php }
