d2865de68379e8f65b1b121946607b7f184beb35
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-12 09:26:56 -0300

    Adding custom query in front-page.php

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -14,7 +14,15 @@
         <div class="full-width-split__one">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-
+            <?php
+                $homepageEvents = new WP_Query(array(
+                    'posts_per_page' => 2,
+                    'post_type' => 'event'
+                )); 
+                while($homepageEvents->have_posts()){
+                    $homepageEvents->the_post();
+                }
+            ?>
             <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
                 <span class="event-summary__month">Mar</span>
