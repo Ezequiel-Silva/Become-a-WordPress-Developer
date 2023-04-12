e0e02b8e6cf6036fa40b71e8fdb729b216ac6e3f
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-12 13:32:32 -0300

    site_url('/events') in archive-event.php

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -14,7 +14,7 @@
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                    <a class="metabox__blog-home-link" href="<?php site_url('/blog'); ?>">
+                    <a class="metabox__blog-home-link" href="<?php site_url('/events'); ?>">
                         <i class="fa fa-home" aria-hidden="true"></i> 
                         Events Home 
                     </a> 
