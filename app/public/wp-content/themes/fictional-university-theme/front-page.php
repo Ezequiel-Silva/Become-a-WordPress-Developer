6b977c3b136be6dca638d3b510b17a6889015bd7
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-13 17:56:21 -0300

    Adding event day format in front-page.php

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -27,7 +27,11 @@
                                 $eventDate = new DateTime(get_field('event_date'));
                                 echo $eventDate->format('M');
                             ?></span>
-                            <span class="event-summary__day">25</span>
+                            <span class="event-summary__day">
+                                <?php 
+                                    echo $eventDate->format('d');
+                                ?>
+                            </span>
                         </a>
                         <div class="event-summary__content">
                             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
