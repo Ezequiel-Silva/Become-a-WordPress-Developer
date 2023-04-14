8ebc0893f7319c404c39696dc68ba11a37812025
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-14 10:36:21 -0300

    Bring all posts that meet the condition ser in front-page.php

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -16,7 +16,7 @@
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
             <?php
                 $homepageEvents = new WP_Query(array(
-                    'posts_per_page' => 2,
+                    'posts_per_page' => -1,
                     'post_type' => 'event',
                     'orderby' => 'title'
                 )); 
