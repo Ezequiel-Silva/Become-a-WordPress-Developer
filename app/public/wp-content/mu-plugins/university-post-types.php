8e7985e75f7e43c76324c526fd45a95f3e06d657
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-17 17:05:44 -0300

    Register a new publication type program in header.php

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,5 +1,6 @@
 <?php
     function university_post_types(){
+        // Event Post Type
         register_post_type('event', array(
             'supports' => array('title','editor','excerpt'),
             'rewrite' => array('slug' => 'events'),
@@ -14,6 +15,23 @@
                 'singular_name' => 'Evento'
             ),
             'menu_icon' => 'dashicons-calendar'
+        ));
+
+        // Program Post Type
+        register_post_type('program', array(
+            'supports' => array('title','editor'),
+            'rewrite' => array('slug' => 'programs'),
+            'has_archive' => true,
+            'public' => true,
+            'show_in_rest' => true,
+            'labels' => array(
+                'name' => 'Programs',
+                'add_new_item' => 'Adicionar novo programa',
+                'edit_item' => 'Editar programa',
+                'all_items' => 'Todos os programas',
+                'singular_name' => 'Programa'
+            ),
+            'menu_icon' => 'dashicons-awards'
         ));
     }
     add_action('init','university_post_types');
