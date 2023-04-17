4d4e5c32d01939affd1c1fa1c4dab57155049c6d
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-17 12:00:09 -0300

    Limit 1 post per page in page-past-events.php

--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -12,6 +12,7 @@
     <?php
     $today = date('Ymd');
     $pastEvents = new WP_Query(array(
+        'posts_per_page' => 1,
         'post_type' => 'event',
         'meta_key' => 'event_date',
         'orderby' => 'meta_value_num',
