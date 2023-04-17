3712ccb06d33cb3b2df586c8822e091760309866
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-17 14:22:16 -0300

    Dynamic pagination when there are more than 10 posts in page-past-events.php

--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -13,7 +13,6 @@
     $today = date('Ymd');
     $pastEvents = new WP_Query(array(
         'paged' => get_query_var('paged',1),
-        'posts_per_page' => 1,
         'post_type' => 'event',
         'meta_key' => 'event_date',
         'orderby' => 'meta_value_num',
