693f43e1e0e60388a4b3a676145803d8daebb9a8
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-17 16:23:53 -0300

    Current menu item in header.php

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="
         <?php bloginfo( 'charset' ); ?>" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <?php wp_head();?>
     </head>
     <body <?php body_class(); ?>>
@@ -22,7 +22,7 @@
                         <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 15) echo 'class="current-menu-item"' ?>
                         ><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                         <li><a href="#">Programs</a></li>
-                        <li <?php if(get_post_type() == 'event') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
+                        <li <?php if(get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
                         <li><a href="#">Campuses</a></li>
                         <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
                     </ul>
@@ -35,4 +35,3 @@
             </div>
             </div>
         </header>
-
