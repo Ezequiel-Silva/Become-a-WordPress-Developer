87926d82639927afea0c2854351524b6b0cd3b24
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-18 15:18:20 -0300

    Adding title and elements to program title in single-event.php

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -29,9 +29,13 @@
             <?php 
             $relatedPrograms = get_field('related_programs');
             //print_r($relatedPrograms);
-            foreach($relatedPrograms as $program){
-                echo get_the_title($program);
-            }
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+            echo '<ul class="link-list min-list">';
+            foreach($relatedPrograms as $program){ ?>
+                <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+            <?php }
+            echo '</ul>';
             ?>
         </div>
     <?php }
