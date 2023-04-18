f033484645f63bf3c11053a790afb4c0249bb024
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-18 15:43:47 -0300

    Show title, when event has at least one program selected in single-event.php

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -29,13 +29,15 @@
             <?php 
             $relatedPrograms = get_field('related_programs');
             //print_r($relatedPrograms);
-            echo '<hr class="section-break">';
-            echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
-            echo '<ul class="link-list min-list">';
-            foreach($relatedPrograms as $program){ ?>
+            if($relatedPrograms){
+                echo '<hr class="section-break">';
+                echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+                echo '<ul class="link-list min-list">';
+                foreach($relatedPrograms as $program){ ?>
                 <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
-            <?php }
-            echo '</ul>';
+                <?php }
+                echo '</ul>';
+            }
             ?>
         </div>
     <?php }
