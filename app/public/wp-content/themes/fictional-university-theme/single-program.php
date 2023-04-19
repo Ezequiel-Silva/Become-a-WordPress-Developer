1e2918460bb0220dd5473ebcf69365cd910fc3b6
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-19 13:52:06 -0300

    Updating level 2 title in single-program.php

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -43,7 +43,7 @@
                 )); 
                 if($relatedProfessors->have_posts()){
                     echo '<hr class="section-break">';
-                    echo '<h2 class="headline headline--medium">Upcoming '.get_the_title().' Events</h2>';
+                    echo '<h2 class="headline headline--medium">'.get_the_title().' Professors</h2>';
                 while($relatedProfessors->have_posts()){
                     $relatedProfessors->the_post();?>
                     <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
