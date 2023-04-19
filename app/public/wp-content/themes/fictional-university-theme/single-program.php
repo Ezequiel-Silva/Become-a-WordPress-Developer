ba227ea8ab76835fed1449e42b8194ee96d58503
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-19 13:48:00 -0300

    Personalized query for program related teachers in single-program.php

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -27,6 +27,29 @@
                 <?php the_content(); ?>
             </div>
             <?php
+                //
+                $relatedProfessors = new WP_Query(array(
+                    'posts_per_page' => -1,
+                    'post_type' => 'professor',
+                    'orderby' => 'title',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'related_programs',
+                            'compare' => 'LIKE',
+                            'value' => '"' . get_the_ID() . '"'
+                        )
+                    )
+                )); 
+                if($relatedProfessors->have_posts()){
+                    echo '<hr class="section-break">';
+                    echo '<h2 class="headline headline--medium">Upcoming '.get_the_title().' Events</h2>';
+                while($relatedProfessors->have_posts()){
+                    $relatedProfessors->the_post();?>
+                    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                <?php }
+                }
+                //
                 $today = date('Ymd');
                 $homepageEvents = new WP_Query(array(
                     'posts_per_page' => 2,
