ad1879c8663b1cd5b86174de694e4d7894437deb
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-20 13:41:31 -0300

    Putting the image with the professorPortrait size in single-professor.php

--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -16,7 +16,7 @@
             <div class="generic-content">
                 <div class="row group">
                     <div class="one-third">
-                        <?php the_post_thumbnail(); ?>
+                        <?php the_post_thumbnail('professorPortrait'); ?>
                     </div>
                     <div class="two-thirds">
                         <?php the_content(); ?>
