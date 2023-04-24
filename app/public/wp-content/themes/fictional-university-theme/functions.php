e112f9c3530e6b16769746ad908d10e05361ef72
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-24 10:58:32 -0300

    Adding two conditions in if related to banner image custom field in function.php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -7,7 +7,7 @@
             $args['subtitle'] = get_field('page_banner_subtitle');
         }
         if(!isset($args['photo'])){
-            if(get_field('page_banner_background_image')){
+            if(get_field('page_banner_background_image') AND !is_archive() AND !is_home()){
                 $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
             } else {
                 $args['photo'] = get_theme_file_uri('images/ocean.jpg');
