271cf75dd45b48722ee6b788a4fed87e00e18709
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-24 10:48:42 -0300

    Fixing undefined array key error with isset() in functions.php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,12 +1,12 @@
 <?php
     function pageBanner($args = null){
-        if(!$args['title']){
+        if(!isset($args['title'])){
             $args['title'] = get_the_title();
         }
-        if(!$args['subtitle']){
+        if(!isset($args['subtitle'])){
             $args['subtitle'] = get_field('page_banner_subtitle');
         }
-        if(!$args['photo']){
+        if(!isset($args['photo'])){
             if(get_field('page_banner_background_image')){
                 $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
             } else {
