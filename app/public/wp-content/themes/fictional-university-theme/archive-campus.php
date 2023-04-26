80dce415e64e414a96af996c6d81d57e64df8ad1
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2023-04-26 15:47:02 -0300

    Initial structure of archive-campus.php

--- app/public/wp-content/themes/fictional-university-theme/archive-campus.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-campus.php
@@ -1,1 +1,17 @@
-hello 123
+<?php get_header(); 
+pageBanner(array(
+    'title' => 'Our Campusus',
+    'subtitle' => 'We have several conveniently located campusus'
+));
+?>
+<div class="container container–narrow page-section">
+    <ul class="link-list min-list">
+        <?php while(have_posts()){ 
+            the_post(); ?>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php }
+        echo paginate_links();
+        ?>
+    </ul>
+</div>
+<?php get_footer(); ?>
